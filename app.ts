45fa35613990ef51791c18725cd04986380db49d
20130143 <vszzvs@naver.com>
2021-12-14 18:53:40 +0900

    [생성] Ts 프로잭트 생성

---
+++ app.ts
@@ -0,0 +1,7 @@
+import * as express from "express"
+
+const app = express();
+app.get("/",(req : express.Request , res : express.Response) =>{
+  res.send("start");
+})
+app.listen(4000,()=>console.log("start"));
