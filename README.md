a30fa12897e0e6ce9ee4d76f1ed017020471f319
lebania <hdodo777@gmail.com>
2022-02-07 18:41:23 +0900

    [수정해야됨] 공부해야되는거 메모 + 드가자

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 * Passport local 로그인, kakao 로그인
 * 회원 가입
 * 판매 포스팅 가능
+
+
+
+
+# 봐야 되는거 확인해야된다...
+https://vincit.github.io/objection.js/guide/query-examples.html#basic-queries
+https://knexjs.org/#typescript-support
+https://github.com/colinhacks/zod#primitives (zod)
+lodash 확인해야됨
--- router/salepost.ts
+++ router/salepost.ts
@@ -1,9 +1,10 @@
 import {Router} from "express"
 import controllersalepost from "../controller/salepost";
+import { z } from "zod"
 
 const router = Router();
 
-router.route("/").get(controllersalepost.getsalepost).post(controllersalepost.postsalepost);
+router.route("/").get( controllersalepost.getsalepost).post(controllersalepost.postsalepost);
 router.route("/:id").get(controllersalepost.onesalepost);
 
 export default router;
